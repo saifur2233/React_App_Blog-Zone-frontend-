060d1cc44af5e244eb4ffa9972ff2788a4944905
saifur2233 <saifurnstuiit223344@gmail.com>
2022-07-08 14:41:43 +0600

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar/navbar'
-import Home from './components/Home/home'
-import Contact from './components/Contact/contact'
-import Error from './components/Error/error'
-import About from './components/About/about'
-import AllBlogs from './components/Blogs/allBlogs'
-import CreateBlog from './components/Blogs/createblog'
-import SignIn from './components/Authentication/signin'
-import SignUp from './components/Authentication/signup'
-import Footer from './components/Footer/footer'
+import Navbar from './components/Navbar/navbar';
+import Home from './components/Home/home';
+import Contact from './components/Contact/contact';
+import Error from './components/Error/error';
+import About from './components/About/about';
+import AllBlogs from './components/Blogs/allBlogs';
+import CreateBlog from './components/Blogs/createblog';
+import SignIn from './components/Authentication/signin';
+import SignUp from './components/Authentication/signup';
+import TestBlog from './components/Test/allBlogs';
+import Footer from './components/Footer/footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={ <Home />} />
-      <Route path="/blogs" element={ <AllBlogs />} />
-      <Route path="/post" element={ <CreateBlog />} />
-      <Route path="/about" element={ <About />} />
-      <Route path="/contact" element={ <Contact />} />
-      <Route path="/signin" element={ <SignIn />} />
-      <Route path="/signup" element={ <SignUp />} />
-      <Route path="*" element={ <Error />} />
-    </Routes>
-    <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<AllBlogs />} />
+        <Route path="/post" element={<CreateBlog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/test" element={<TestBlog />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
